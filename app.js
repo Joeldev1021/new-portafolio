6193fd3eee9c8f828f799454e2eb6e1dbc2b2d22
Joel Pimentel <joelpimentel1995@gmail.com>
2021-09-05 10:20:09 -0500

    styles menu

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const $btnMenu = document.getElementById('btn__menu')
 const $nav = document.querySelector('.menu')
 
-$btnMenu.addEventListener('click', ()=>{
+$btnMenu.addEventListener('click', (e)=>{
+    if(!$nav.classList.contains('show')){
+        e.target.classList.toggle('far-bars')
+        e.target.classList.add('fa-times')
+    }else {
+        e.target.classList.remove("fa-times")
+        e.target.classList.add('far-bars')
+    }
     $nav.classList.toggle('show')
+    
 })
+
+ScrollReveal().reveal('.main');
+ScrollReveal().reveal('.about',{ delay: 600 });
+ScrollReveal().reveal('.skills',{ delay: 600 });
+ScrollReveal().reveal('.proyect',{ delay: 600 });
+ScrollReveal().reveal('.contact',{ delay: 600 });
